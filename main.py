73802bef4baff668d04daa264d3b992bcce36583
moalhusaini <alhusainimohammad21@gmail.com>
2023-01-31 10:56:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+x = float(input("can you give me the length of the rectangle?"))
+y = float(input("can you give me the width of the rectangle?"))
+area = y * x
+print("the area of the rectangle is")
+print(area)
